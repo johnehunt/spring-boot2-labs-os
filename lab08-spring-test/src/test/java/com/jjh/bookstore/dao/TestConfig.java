7157951879a7bc8d6202184436cbe50a0cb9358d
John Hunt <john.hunt10@gmail.com>
2021-11-02 10:50:36 +0000

    Sample solution to lab 8

---
+++ lab08-spring-test/src/test/java/com/jjh/bookstore/dao/TestConfig.java
@@ -0,0 +1,8 @@
+package com.jjh.bookstore.dao;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.jjh.bookstore.dao"})
+public class TestConfig {}
